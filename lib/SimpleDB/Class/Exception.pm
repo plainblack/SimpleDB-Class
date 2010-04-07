c4e0f847ccd125739ab6c823b483cb43f9a49a3d
JT Smith <jt@plainblack.com>
2010-04-07 11:49:53 -0500

    successfully converted to SimpleDB::Client

--- lib/SimpleDB/Class/Exception.pm
+++ lib/SimpleDB/Class/Exception.pm
@@ -16,10 +16,6 @@ The following exceptions are available from this class.
 
 A general error. Isa Exception::Class.
 
-=head2 SimpleDB::Class::Exception::OverrideMe
-
-Used when creating abstract methods. Isa SimpleDB::Class::Exception.
-
 =head2 SimpleDB::Class::Exception::ObjectNotFound
 
 Thrown when a request object is not found.
@@ -44,26 +40,6 @@ Thrown when exceptions occur connecting to the SimpleDB database at Amazon, or t
 
 The HTTP status code returned.
 
-=head2 SimpleDB::Class::Exception::Response
-
-Isa SimpleDB::Class::Exception::Connection. Thrown when SimpleDB reports an error.
-
-=head3 error_code
-
-The error code returned from SimpleDB.
-
-=head3 request_id
-
-The request id as returned from SimpleDB.
-
-=head3 box_usage
-
-The storage usage in your SimpleDB.
-
-=head3 response
-
-The L<HTTP::Response> object as retrieved from the SimpleDB request.
-
 =cut
 
 use strict;
@@ -71,10 +47,6 @@ use Exception::Class (
 
     'SimpleDB::Class::Exception' => {
         description     => "A general error occured.",
-        },
-    'SimpleDB::Class::Exception::OverrideMe' => {
-        isa             => 'SimpleDB::Class::Exception',
-        description     => 'This method should be overridden by subclasses.',
         },
     'SimpleDB::Class::Exception::InvalidParam' => {
         isa             => 'SimpleDB::Class::Exception',
@@ -94,11 +66,6 @@ use Exception::Class (
         isa             => 'SimpleDB::Class::Exception',
         description     => "There was a problem establishing a connection.",
         fields          => ['status_code'],
-        },
-    'SimpleDB::Class::Exception::Response' => {
-        isa             => 'SimpleDB::Class::Exception::Connection',
-        description     => "The database reported an error.",
-        fields          => ['error_code','request_id','box_usage','response'],
         },
 
 );
